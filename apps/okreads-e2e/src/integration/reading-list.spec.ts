c059a802d17ea26aec50a31e21c4dc92c96c6b2a
Hiten Sharma <shiten@pkrmbendc.com>
2022-02-23 01:04:18 +0530

    test cases

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,42 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should undo my actions', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
+    cy.get('.want-to-read').first().click();
+    cy.get('.want-to-read').first().should('be.disabled');
+
+    cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
+      'contain.text',
+      'Added'
+    );
+    cy.get(
+      '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
+    ).click();
+    cy.get('.want-to-read').first().should('not.be.disabled');
+    cy.get('.want-to-read').first().click();
+    cy.get('.want-to-read').first().should('be.disabled');
+
+    cy.get('[data-testing="reading-list-item"]').should(
+      'have.length.greaterThan',
+      0
+    );
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('.reading-list-item > .reading-list-button > button').click();
+    cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
+      'contain.text',
+      'Removed'
+    );
+    cy.get(
+      '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
+    )
+      .first()
+      .click();
+    cy.get('.want-to-read').first().should('not.be.disabled');
+    cy.get('.want-to-read').first().click();
+    cy.get('.want-to-read').first().should('be.disabled');
+  });
 });
