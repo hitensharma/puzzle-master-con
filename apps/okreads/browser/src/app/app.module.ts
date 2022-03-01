c00c98417dd3fafa8eac8dfbe7230e18c6a7a8d1
Hiten Sharma <shiten@pkrmbendc.com>
2022-03-02 02:03:31 +0530

    unit test cases added

--- apps/okreads/browser/src/app/app.module.ts
+++ apps/okreads/browser/src/app/app.module.ts
@@ -5,36 +5,37 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BooksFeatureModule } from '@tmo/books/feature';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    BooksFeatureModule,
     MatBadgeModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
+    BooksFeatureModule,
     StoreModule.forRoot(
       {},
       {
         runtimeChecks: {
           strictStateImmutability: true,
-          strictActionImmutability: true,
-        },
+          strictActionImmutability: true
+        }
       }
     ),
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ name: 'tmo' }),
+    StoreDevtoolsModule.instrument({ name: 'tmo' })
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- libs/books/feature/src/lib/message.service.spec.ts
+++ libs/books/feature/src/lib/message.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MessageService } from './message.service';
+import { Store } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 
 describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule, StoreModule.forRoot({})],
+      providers: [MessageService, Store],
+    });
     service = TestBed.inject(MessageService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeDefined();
   });
 });
