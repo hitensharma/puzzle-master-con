05ba627729533356b170e7949e39d71c7c1d21a5
Hiten Sharma <shiten@pkrmbendc.com>
2022-03-02 03:39:01 +0530

    e2e test cases

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -5,7 +5,6 @@ describe('When: I use the reading list feature', () => {
 
   it('Then: I should see my reading list', () => {
     cy.get('[data-testing="toggle-reading-list"]').click();
-
     cy.get('[data-testing="reading-list-container"]').should(
       'contain.text',
       'My Reading List'
@@ -16,125 +15,76 @@ describe('When: I use the reading list feature', () => {
     cy.get('input[type="search"]').type('javascript');
     cy.get('form').submit();
     cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
-
     cy.get('.want-to-read')
       .first()
       .then(($btn) => {
         if ($btn.is(':disabled')) {
-          // ? ADD TO READING LIST UNDO BUTTON
           cy.get('[data-testing="toggle-reading-list"]').click();
           cy.get('.reading-list-item').each(() => {
             cy.get('[data-testing="remove-button"]').first().click();
           });
-
           cy.get('[data-testing="reading-list-item"]').should(
             'have.length.at.most',
             0
           );
           cy.get('[data-testing="close-reading-list"]').click();
-
-          cy.get('.want-to-read').first().click();
-          cy.get('.want-to-read').first().should('be.disabled');
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
-          cy.get('[data-testing="close-reading-list"]').click();
-
-          cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
-            'contain.text',
-            'Added'
-          );
-          cy.get(
-            '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
-          ).click();
-          cy.get('.want-to-read').first().should('not.be.disabled');
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.at.most',
-            0
-          );
-
-          // ? REMOVE FROM READING LIST UNDO BUTTON
-          cy.get('.want-to-read').first().click();
-          cy.get('.want-to-read').first().should('be.disabled');
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
-          cy.get('[data-testing="close-reading-list"]').click();
-          cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
-            'contain.text',
-            'Added'
-          );
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('[data-testing="remove-button"]').first().click();
-          cy.get('[data-testing="close-reading-list"]').click();
-          cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
-            'contain.text',
-            'Removed'
-          );
-          cy.get(
-            '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
-          ).click();
-          cy.get('.want-to-read').first().should('be.disabled');
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
+         checkUndo()
         } else {
-          // ? ADD TO READING LIST UNDO BUTTON
-          cy.get('.want-to-read').first().click();
-          cy.get('.want-to-read').first().should('be.disabled');
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
-
-          cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
-            'contain.text',
-            'Added'
-          );
-          cy.get(
-            '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
-          ).click();
-          cy.get('.want-to-read').first().should('not.be.disabled');
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.at.most',
-            0
-          );
-
-          // ? REMOVE FROM READING LIST UNDO BUTTON
-
-          cy.get('.want-to-read').first().click();
-          cy.get('.want-to-read').first().should('be.disabled');
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
-          cy.get('[data-testing="close-reading-list"]').click();
-          cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
-            'contain.text',
-            'Added'
-          );
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('[data-testing="remove-button"]').first().click();
-          cy.get('[data-testing="close-reading-list"]').click();
-          cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
-            'contain.text',
-            'Removed'
-          );
-          cy.get(
-            '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
-          ).click();
-          cy.get('.want-to-read').first().should('be.disabled');
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
+          checkUndo()
         }
       });
   });
+
+  
+
+  function checkUndo(){
+    // ? ADD TO READING LIST UNDO BUTTON
+    cy.get('.want-to-read').first().click();
+    cy.get('.want-to-read').first().should('be.disabled');
+    cy.get('[data-testing="reading-list-item"]').should(
+      'have.length.greaterThan',
+      0
+    );
+    cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
+      'contain.text',
+      'Added'
+    );
+    cy.get(
+      '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
+    ).last().click();
+    cy.get('.want-to-read').first().should('not.be.disabled');
+    cy.get('[data-testing="reading-list-item"]').should(
+      'have.length.at.most',
+      0
+    );
+
+    // ? REMOVE FROM READING LIST UNDO BUTTON
+    cy.get('.want-to-read').first().click();
+    cy.get('.want-to-read').first().should('be.disabled');
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('[data-testing="reading-list-item"]').should(
+      'have.length.greaterThan',
+      0
+    );
+    cy.get('[data-testing="close-reading-list"]').click();
+    cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
+      'contain.text',
+      'Added'
+    );
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('[data-testing="remove-button"]').first().click();
+    cy.get('[data-testing="close-reading-list"]').click();
+    cy.get('.undo-dialog > .mat-simple-snackbar > span').should(
+      'contain.text',
+      'Removed'
+    );
+    cy.get(
+      '.undo-dialog > .mat-simple-snackbar > .mat-simple-snackbar-action > button'
+    ).click();
+    cy.get('.want-to-read').first().should('be.disabled');
+    cy.get('[data-testing="reading-list-item"]').should(
+      'have.length.greaterThan',
+      0
+    );
+  }
 });
