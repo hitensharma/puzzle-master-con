825f2a337264e5cd1efbc1399781373734ce9254
Hiten Sharma <shiten@pkrmbendc.com>
2022-03-02 03:16:20 +0530

    e2e test cases

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -21,74 +21,51 @@ describe('When: I use the reading list feature', () => {
       .first()
       .then(($btn) => {
         if ($btn.is(':disabled')) {
+          // IF BUTTON IS DISABLED
           cy.get('[data-testing="toggle-reading-list"]').click();
           cy.get('.reading-list-item').each(() => {
             cy.get('[data-testing="remove-button"]').first().click();
           });
-
           cy.get('[data-testing="reading-list-item"]').should(
             'have.length.at.most',
             0
           );
           cy.get('[data-testing="close-reading-list"]').click();
+          markBook();
+        } else {
+          // IF BUTTON IS NOT DISABLED
+          markBook();
+        }
+      });
+  });
 
-          cy.get('.want-to-read').first().click();
-          cy.get('.want-to-read').first().should('be.disabled');
-
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('[data-testing="finish-button"]').first().click({force:true});
+  
+  function markBook() {
+    cy.get('.book-grid').each(() => {
+      cy.get('.want-to-read').click({ multiple: true });
+      cy.get('.want-to-read').should('be.disabled');
+    });
+    cy.get('[data-testing="reading-list-item"]').should(
+      'have.length.greaterThan',
+      0
+    );
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('.reading-list-item').each(() => {
+      cy.get('[data-testing="finish-button"]').then(($readBtn) => {
+        if ($readBtn.is(':disabled')) {
+          cy.get('[data-testing="remove-button"]').first().click();
+        } else {
+          cy.get('[data-testing="finish-button"]').click({
+            multiple: true,
+          });
 
           cy.get('[data-testing="reading-list-item"]').should(
             'contain.text',
             'Finished'
           );
           cy.get('[data-testing="remove-button"]').first().click();
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.at.most',
-            0
-          );
-        } else {
-          cy.get('.book-grid').each(() => {
-            cy.get('.want-to-read').click({ multiple: true });
-            cy.get('.want-to-read').should('be.disabled');
-          });
-
-          cy.get('[data-testing="reading-list-item"]').should(
-            'have.length.greaterThan',
-            0
-          );
-          cy.get('[data-testing="toggle-reading-list"]').click();
-          cy.get('.reading-list-item').each(() => {
-            // cy.get('[data-testing="finish-button"]').click({ multiple: true });
-
-            // cy.get('[data-testing="reading-list-item"]').should(
-            //   'contain.text',
-            //   'Finished'
-            // );
-
-            cy.get('[data-testing="finish-button"]')
-              
-              .then(($readBtn) => {
-                if ($readBtn.is(':disabled')) {
-                  cy.get('[data-testing="remove-button"]').first().click();
-                } else {
-                  cy.get('[data-testing="finish-button"]').click({
-                    multiple: true
-                  });
-
-                  cy.get('[data-testing="reading-list-item"]').should(
-                    'contain.text',
-                    'Finished'
-                  );
-                  cy.get('[data-testing="remove-button"]').first().click();
-                }
-              });
-          });
         }
       });
-  });
+    });
+  }
 });
